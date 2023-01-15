9109da270c4536328ceb74994803b47575b16418
benjamin chukwudi <73422820+benjaminugwuanyi@users.noreply.github.com>
2023-01-15 08:16:16 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://thrivest:HelloToday12@cluster0.caweziw.mongodb.net/ThrivestUsers'
+module.exports = {
+    mongoURI: dbPassword
+};
